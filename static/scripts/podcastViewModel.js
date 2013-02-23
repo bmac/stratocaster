04c53f9d3240cbc1033c0253471b9ef2824935a3
bmac <bmac325@gmail.com>
2013-02-23 13:50:37 -0500

    filter the episode list to show only unlistened episodes

--- static/scripts/podcastViewModel.js
+++ static/scripts/podcastViewModel.js
@@ -1,7 +1,11 @@
 define(['baseViewModel', 'knockout-2.2.1', 'vent'], function(BaseViewModel, ko, vent) {
     var url = '/resources/podcast/:podcast_id/episode/:episode_id/listened_to/';
     var podcastViewModel = BaseViewModel.extend({
+	defaults: {
+	    filtered: true
+	},
 	init: function(podcast) {
+	    var self = this;
 	    podcast.episode_set.forEach(function(episode) {
 		episode.listened_to = ko.observable(episode.listened_to);
 		episode.listened_to.subscribe(function(new_value) {
@@ -11,7 +15,16 @@ define(['baseViewModel', 'knockout-2.2.1', 'vent'], function(BaseViewModel, ko, 
 		    $.post(update_url, {listened_to: new_value});
 		});
 	    });
-	    this._super(podcast);
+	    self._super(podcast);
+
+	    self.episodes = ko.computed(function() {
+		if (self.filtered()) {
+		    return self.episode_set().filter(function(episode) {
+			return episode.listened_to() === false;
+		    });
+		}
+		return self.episode_set();
+	    });
 	},
 	playPodcast: function(episode) {
 	    vent.trigger('playAudio', episode.link);
